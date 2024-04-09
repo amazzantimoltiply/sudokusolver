b4e08c84420ede6f33e93f51cb441cf5145a2639
Gammaglobulino <mazzantia@hotmail.com>
2024-04-09 16:55:41 +0200

    my work on haskell

--- soarwithhaskell/app/Main.hs
+++ soarwithhaskell/app/Main.hs
@@ -29,7 +29,7 @@ instance Eq Expr where
     Var s1 == Var s2 = s1 == s2
     Lit n == Lit m = n == m
     Add e1 e2 == Add e3 e4 = e1 == e2 && e3 == e4
-    e1 == e2 = eval e1 == eval e2
+    e1 == e2 = eval e1 == eval e2 
     _ == _ = False
 
 
@@ -39,11 +39,11 @@ main =
     --print (insertGen (>=) [1,3,4,5] 6)
     --print (iSortGen (<) [3,6,7,2,3,0,9])
     --print (summWithFold [1,2,3,4,5])
-    --print (myScanR (+) 0 [0,1,2,3,4,5])
+    print (myScanR (-) 0 [0,1,2,3,4,5])
     --print (average [1,2,3,6,10,6,8,3,5]) 
     --print (zip scaffold (tails' (scanl (+) 0 (map price scaffold ))))
     --print (map price [MkP "medicine" 8, MkP "rice" 1, MkP "sweets" 3])
     --print (eval expr list)
-    print (Lit 3 == Add (Lit 1) (Lit 2) )
+    --print (Lit 3 == Add (Lit 1) (Lit 2) )
     --print (deleteSame [1,2,3,1,4,5,6,1,3] 1 )
 
