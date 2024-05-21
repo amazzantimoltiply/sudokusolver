eaa20e8d9f90178ffb305a6eed68384b7e435728
Gammaglobulino <mazzantia@hotmail.com>
2024-05-21 16:04:04 +0200

    mutate a text file

--- playwithfoldable/app/Main.hs
+++ playwithfoldable/app/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveFoldable #-}
 module Main (main) where
+import Data.Foldable
 
 data Tree a = Leaf a | Fork (Tree a) (Tree a) deriving Foldable
 
@@ -9,5 +10,5 @@ treeToList (Fork l r) = treeToList l ++ treeToList r
 
 tree= Fork (Leaf 2) (Leaf 3)
 main :: IO ()
-main = do print (treeToList tree)
-
+--main = do print (sum tree)
+main = do print (map (+) (Fork (Leaf 2) (Leaf 3)))
