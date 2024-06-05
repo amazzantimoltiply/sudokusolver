503e9230d5f9deb8b018de15b19aa90b11a65b6c
Gammaglobulino <mazzantia@hotmail.com>
2024-06-05 14:27:44 +0200

    review EH chap 2

--- EffectiveHaskell/app/Main.hs
+++ EffectiveHaskell/app/Main.hs
@@ -6,6 +6,14 @@ data List a = Nil | Cons a (List a)
 lenghList::List a -> Int
 lenghList Nil = 0
 lenghList (Cons x xs) = 1 + lenghList xs
+verifyNumber num
+  | num > 0 = let positive = "Positive" in exclaim positive
+  | num < 0 = let negative = "Negative" in exclaim negative
+  | num < 3 = exclaim "Small"
+  | num < 100 = let medium = "Medium" in exclaim medium
+  | otherwise = exclaim "Large"
+  where
+    exclaim str = show num <>" is " <> str <> " "  
 
 main :: IO()
 main =
@@ -18,7 +26,8 @@ main =
     -- print (fact 10)
     -- print (fibonacci 10)
     -- print (lenghList (Cons 1 (Cons 2 Nil)))
-    print (tails' [1,2,3,4,5])
+    -- print (tails' [1,2,3,4,5])
+    print (verifyNumber (-3))
 
     
 
