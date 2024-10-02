0e2f92a397f5675254e5ed634da9565cf7095d6d
Gammaglobulino <mazzantia@hotmail.com>
2024-10-02 14:23:44 +0200

    Additional refactoring

--- EffectiveHaskell/app/Main.hs
+++ EffectiveHaskell/app/Main.hs
@@ -13,7 +13,28 @@ verifyNumber num
   | num < 100 = let medium = "Medium" in exclaim medium
   | otherwise = exclaim "Large"
   where
-    exclaim str = show num <>" is " <> str <> " "  
+    exclaim str = show num <>" is " <> str <> " "
+
+myZipWith::(a->b->c)->[a]->[b]->[c]
+myZipWith fun as bs = [fun a b | a<- as,b <-bs]
+
+--myZipWithRec::(a->b->c)->[a]->[c]->[c]
+--myZipWithRec _ [] (b:bs) = [b]
+--myZipWithRec _ (a:as) [] = [a]
+--myZipWithRec f (a:as) (b:bs) = f a b : myZipWithRec f as bs
+
+--myZipWithFold::(a->a->a)->[a]->[a]->[a]
+--myZipWithFold f aas bbs = foldr f . streamLists aas bbs  where
+--  streamLists [] (b:bs) = b
+--  streamLists (a:as) [] = a
+--  streamLists (a:as) (b:bs) = a : b : streamLists as bs
+
+
+sumOfDiff xxs = sumBiggest xxs - sumSmallest xxs where
+                    sumBiggest xxs = sum [maximum xs | xs <-xxs]
+                    sumSmallest xxs = sum [minimum xs | xs <-xxs]
+
+
 
 main :: IO()
 main =
@@ -27,7 +48,10 @@ main =
     -- print (fibonacci 10)
     -- print (lenghList (Cons 1 (Cons 2 Nil)))
     -- print (tails' [1,2,3,4,5])
-    print (verifyNumber (-3))
-
-    
-
+    -- print (verifyNumber (-3))
+    -- print (myZipWith (,) [1,2] [1,2])
+    -- print (myZipWithRec (,) $ [1,2] [1,2])
+    --print (myZipWithFold (+) [1,2,3] [1,2,3])
+    --print (sumBiggest [[1,2,3],[1,2,3]])
+    --print (sumBiggest [[1,2,3],[1,2,3]])
+    print (sumOfDiff [[1,2,3],[1,2,3]])
