2b9e0bdb7ffab4384dd5c65c2ad8ea0db2b19b75
Gammaglobulino <mazzantia@hotmail.com>
2025-08-18 10:29:08 +0200

    update merge sort

--- introduction2computation/src/Lib.hs
+++ introduction2computation/src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
     ( primes,
-      upto
+      upto,
+      countDoubled,
+      reverseList,
+      reverseListRecursive
     ) where
 primes :: [Int]
 primes = 2 : [n | n <- [3,5..], isPrime n]
@@ -8,3 +11,18 @@ primes = 2 : [n | n <- [3,5..], isPrime n]
     isPrime n = all (\p -> n `mod` p /= 0) (takeWhile (\p -> p*p <= n) primes)
 upto :: (Ord a) => a -> [a] -> [a]
 upto n = takeWhile (<= n)
+
+countDoubled::String->Int
+countDoubled [] = 0
+countDoubled xs = length [x | (x,y) <- zip xs (tail xs), x==y ]
+
+reverseList :: [a] -> [a]
+reverseList = foldl (flip (:)) []
+
+-- The explicit recursive version for comparison
+reverseListRecursive :: [a] -> [a]
+reverseListRecursive xs = reverseAcc xs []
+  where
+    reverseAcc :: [a] -> [a] -> [a]
+    reverseAcc [] acc = acc
+    reverseAcc (x:xs) acc = reverseAcc xs (x:acc)
--- introduction2computation/test/LibSpec.hs
+++ introduction2computation/test/LibSpec.hs
@@ -1,7 +1,6 @@
 module LibSpec (spec) where
-import Lib (primes,upto)
+import Lib (primes, upto, countDoubled, reverseList, reverseListRecursive)
 import Test.Hspec
-import Lib
 
 spec :: Spec
 spec = do
@@ -25,4 +24,38 @@ spec = do
     
     it "works with floating point numbers" $ do
       upto (3.5 :: Double) [1.0, 2.0, 3.0, 4.0] `shouldBe` [1.0, 2.0, 3.0]
+  describe "counting words inside strings" $ do
+    it "count double words inside a string" $ do
+      countDoubled "aannddrreea" `shouldBe` 5
+
+  describe "reverseList" $ do
+    it "reverses an empty list" $ do
+      reverseList ([] :: [Int]) `shouldBe` []
     
+    it "reverses a single element list" $ do
+      (reverseList [1] :: [Int]) `shouldBe` [1]
+    
+    it "reverses a list of numbers" $ do
+      (reverseList [1..5] :: [Int]) `shouldBe` [5,4,3,2,1]
+    
+    it "reverses a string" $ do
+      reverseList "hello" `shouldBe` "olleh"
+    
+    it "maintains list length" $ do
+      length (reverseList ([1..100] :: [Int])) `shouldBe` 100
+      
+  describe "reverseList implementations comparison" $ do
+    it "both implementations give same results for empty list" $ do
+      (reverseList ([] :: [Int])) `shouldBe` reverseListRecursive []
+    
+    it "both implementations give same results for single element" $ do
+      (reverseList [1] :: [Int]) `shouldBe` reverseListRecursive [1]
+    
+    it "both implementations give same results for list of numbers" $ do
+      (reverseList [1..5] :: [Int]) `shouldBe` reverseListRecursive [1..5]
+    
+    it "both implementations give same results for strings" $ do
+      reverseList "hello" `shouldBe` reverseListRecursive "hello"
+    
+    it "both implementations maintain list length for large lists" $ do
+      length (reverseList ([1..1000] :: [Int])) `shouldBe` length (reverseListRecursive ([1..1000] :: [Int]))
