c943cf71fdfa76d50ed64473041f23ce4a5e9cc1
Gammaglobulino <mazzantia@hotmail.com>
2025-10-01 11:24:18 +0200

    first commit

--- algeabricdatatypes/src/Lib.hs
+++ algeabricdatatypes/src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
+    ( 
+    , Vehicle(..)
+    , Car(..)
+    , Plane(..)
+    , Price(..)
+    , isCar
+    , areCars
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+data Price = Price Int deriving (Show, Eq)
+
+data Vehicle = Car Price | Plane Price deriving (Show, Eq)
+data Car = Punto | Focus deriving (Show, Eq)
+data Plane = Boeing Int | Airbus Int deriving (Show, Eq)
+
+isCar :: Vehicle -> Bool
+isCar (Car _) = True
+isCar _       = False
+
+areCars :: [Vehicle] -> Bool
+areCars = all isCar
--- algeabricdatatypes/test/Spec.hs
+++ algeabricdatatypes/test/Spec.hs
@@ -1,2 +1,24 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "isCar" $ do
+    it "returns True for a Car" $ do
+      isCar (Car (Price 10000)) `shouldBe` True
+
+    it "returns False for a Plane" $ do
+      isCar (Plane (Price 1000000)) `shouldBe` False
+
+  describe "areCars" $ do
+    it "returns True for an empty list" $ do
+      areCars [] `shouldBe` True
+
+    it "returns True for a list containing only Cars" $ do
+      areCars [Car (Price 10000), Car (Price 20000)] `shouldBe` True
+
+    it "returns False for a list containing at least one Plane" $ do
+      areCars [Car (Price 10000), Plane (Price 1000000), Car (Price 20000)] `shouldBe` False
+
+    it "returns False for a list containing only Planes" $ do
+      areCars [Plane (Price 1000000), Plane (Price 2000000)] `shouldBe` False
